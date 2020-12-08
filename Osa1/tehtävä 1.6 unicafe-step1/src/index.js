91f95c7241d7ccaf99c83e7a09a7a1c5e2f164e1
Timo Keränen <timo@timokeranen.com>
2020-12-08 15:11:57 +0200

    Tehtävä 1.6 unicafe-step1 lisäys

---
+++ Osa1/tehtävä 1.6 unicafe-step1/src/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const mainHeader = 'Give feedback'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Header headerProp={mainHeader} />
+      <button onClick={() => setValue(0)}>Bad</button>
+      <button onClick={() => setValue(0)}>Neutral</button>
+      <button onClick={() => setValue(0)}>Good</button>
+    </div>
+  )
+}
+
+const Header = (props) => {
+  return <h1>{props.headerProp}</h1>
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
