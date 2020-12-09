4da9e0df14a914791df5f88900fcee8d246b3696
Timo Keränen <timo@timokeranen.com>
2020-12-09 20:11:42 +0200

    1.6 toimii

--- Osa1/tehtävä 1.6 unicafe-step1/src/index.js
+++ Osa1/tehtävä 1.6 unicafe-step1/src/index.js
@@ -4,24 +4,48 @@ import ReactDOM from 'react-dom'
 const App = () => {
   // tallenna napit omaan tilaansa
   const mainHeader = 'Give feedback'
+  // useState metodilla luodaan komponentille tila. 
+  // good pitää sisällään tilan, setGood on viittaus funktioon jolla arvoa muutetaan jne.
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   console.log('Tehtävä 1.6 Unicafe-step1 suorituksessa')
 
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
   return (
     <div>
       <Header headerProp={mainHeader} />
-      <button onClick={() => setValue(0)}>Bad</button>
-      <button onClick={() => setValue(0)}>Neutral</button>
-      <button onClick={() => setValue(0)}>Good</button>
+      <button onClick={handleBadClick}> Bad</button>
+      <button onClick={handleNeutralClick}>Neutral</button>
+      <button onClick={handleGoodClick}>Good</button>
+      <Header headerProp='statistics' />
+      <Stat text='good' value={good} />
+      <Stat text='neutral' value={neutral} />
+      <Stat text='bad' value={bad}/>
     </div>
   )
 }
 
 const Header = (props) => {
   return <h1>{props.headerProp}</h1>
+}
+
+const Stat = (props) => {
+  return (
+      <p>{props.text} {props.value}</p>
+  )
 }
 
 ReactDOM.render(<App />, 
