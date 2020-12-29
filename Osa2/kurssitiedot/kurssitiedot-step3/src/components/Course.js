2a38aa60ded2e0e29898070bcc3ee6d4f5fcfe16
Timo Keränen <timo@timokeranen.com>
2020-12-29 11:32:37 +0200

    Kommentointia lisätty

--- Osa2/kurssitiedot/kurssitiedot-step3/src/components/Course.js
+++ Osa2/kurssitiedot/kurssitiedot-step3/src/components/Course.js
@@ -4,20 +4,26 @@ const Course = (props) =>
 {
     const {name, id, parts} = props.course
 
-    /*let totalAmount = parts.reduce(function(sum, part){
-      return sum + part.exercises
-    }, 0)*/
+    // Tässä kommentissa jaan alla olevan lausekkeen osiin oman kertaamiseni vuoksi
+    // parts.reduce funktio saa parametrikseen funktion joka saa parametrikseen sum, part
+    // sum on muuttuja jota voi käyttää oman tarkoituksen mukaan.
+    // part on parts[] taulukon solun sisältö joka vaihtuu kun taulukkoa käydään läpi
+    // nuolioperandin jälkeen on (sum, part)-funktion runko jossa lasketaan yhteenlasku
+    // nuolifunktion jälkeen asetetaan aloitusarvo 0, jolla viitataan taulukon soluun josta
+    // reduce funktion suoritus aloitetaan.
+    const totalAmount = parts.reduce((sum, part) => sum + part.exercises, 0)
 
-    const total = parts.reduce( (sum, part) => {
+    // Alla on ylempi reduce-lauseke muutettu pidempään muotoon 
+    /*const total = parts.reduce(function(sum, part){
       console.log('what is happening', sum, part)
       return sum + part.exercises 
-    }, 0)
+    }, 0)*/
 
    return (
        <div>
            <h1 key={id}>{name}</h1>
             {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-            <h4>Total of {total} exercises</h4>
+            <h4>Total of {totalAmount} exercises</h4>
        </div>
    )
 }
