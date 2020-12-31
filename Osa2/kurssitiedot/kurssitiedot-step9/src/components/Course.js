00004f49f2b1067a1307c82e18a27e962d8dd30a
Timo Keränen <timo@timokeranen.com>
2020-12-31 10:52:21 +0200

    Toimiva versio yhdellä varoituksella

--- Osa2/kurssitiedot/kurssitiedot-step9/src/components/Course.js
+++ Osa2/kurssitiedot/kurssitiedot-step9/src/components/Course.js
@@ -2,30 +2,32 @@ import React from 'react'
 
 const Course = (props) =>
 {
-    const {name, id, parts} = props.course
-
-    // Tässä kommentissa jaan alla olevan lausekkeen osiin oman kertaamiseni vuoksi
-    // parts.reduce funktio saa parametrikseen funktion joka saa parametrikseen sum, part
-    // sum on muuttuja jota voi käyttää oman tarkoituksen mukaan.
-    // part on parts[] taulukon solun sisältö joka vaihtuu kun taulukkoa käydään läpi
-    // nuolioperandin jälkeen on (sum, part)-funktion runko jossa lasketaan yhteenlasku
-    // nuolifunktion jälkeen asetetaan aloitusarvo 0, jolla viitataan taulukon soluun josta
-    // reduce funktion suoritus aloitetaan.
-    const totalAmount = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-    // Alla on ylempi reduce-lauseke muutettu pidempään muotoon 
-    /*const total = parts.reduce(function(sum, part){
-      console.log('what is happening', sum, part)
-      return sum + part.exercises 
-    }, 0)*/
+  console.log('Course props in Course.js:', props.courses[0])
+  //{props.courses.map(element => <CourseElements courseElement = {element}/> )}
+  // Toimiva alla
+  // <CourseElements course = {props.courses[0]} />
 
    return (
-       <div>
-           <h1 key={id}>{name}</h1>
-            {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-            <h4>Total of {totalAmount} exercises</h4>
-       </div>
+      <div key={props.id}>
+        {props.courses.map(courses => < CourseElements course = {courses} /> )}
+      </div>
    )
+}
+
+const CourseElements = (props) =>
+{
+  console.log('CourseElements', props.course)
+  const {name, id, parts} = props.course
+  console.log('CourseElements props name: ', name, 'id: ', id, 'parts:', parts)
+  const totalAmount = parts.reduce((sum, part) => sum + part.exercises, 0)
+
+  return (
+    <>
+      <h2 key={id}>{name}</h2>
+      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+      <h4 key={8}>Total of {totalAmount} exercises</h4>
+    </>
+  )
 }
 
 export default Course
--- Osa2/kurssitiedot/kurssitiedot-step9/src/index.js
+++ Osa2/kurssitiedot/kurssitiedot-step9/src/index.js
@@ -5,34 +5,57 @@ import Course from './components/Course'
 const App = () => {
 
   // Kommenteissa on kirjoittamiani muistiinpanoja, eikä tavanomaisia kommentteja
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  console.log(course)
+  console.log('index.js courses:', courses)
 
-  // Course komponentti saa course nimisen propsin arvoksi olion course
   return (
     <div>
-      <Course course={course} />
+      <h1>Web development curriculum</h1>
+      <Course courses={courses} />
     </div>
   )
 }
