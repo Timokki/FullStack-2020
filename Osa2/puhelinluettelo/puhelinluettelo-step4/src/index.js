76390b46658f2d2cdfa187105a2e6a0b5bac6129
Timo Keränen <timo@timokeranen.com>
2021-01-05 22:26:03 +0200

    puhelinluettelo step-4 toimii

---
+++ Osa2/puhelinluettelo/puhelinluettelo-step4/src/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+      { name: 'Arto Hellas', number: '040-123456' },
+      { name: 'Ada Lovelace', number: '39-44-5323523' },
+      { name: 'Dan Abramov', number: '12-43-234345' },
+      { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
+
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+
+  const handleNameChange = (event) => {
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    //console.log(event.target.value)
+    setNewFilter(event.target.value)
+    console.log('Filtered array: ', filteredPersons)
+  }
+
+  const addRecord = (event) => {
+    event.preventDefault()
+    //console.log('button clicked', event.target)
+
+    if (persons.every(isNameInArray))
+    {
+      const personObject = {
+        name: newName,
+        phoneNr: newNumber
+      }
+      //console.log('setPerson name: ', newName)
+      setPersons(persons.concat(personObject))
+      setNewName('')
+      setNewNumber('')
+    }
+    else 
+      window.alert(`${newName} is already added to phonebook`)
+  }
+
+  const isNameInArray = (person) => person.name !== newName;
+
+  let filteredPersons = persons.filter(person => person.name.includes(newFilter))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+          filter: <input value={newFilter} onChange={handleFilterChange}/>
+      </div>
+      <h2>Add new</h2>
+      <form onSubmit={addRecord}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {filteredPersons.map(person => <p key={person.name}> {person.name} {person.phoneNr} </p>)}
+    </div>
+  )
+
+}
+
+ReactDOM.render(<App /> , document.getElementById('root'))
